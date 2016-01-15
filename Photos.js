b07c4d2aec49a85f5efd37f44e7fd21e03bed6f8
Anonymous <andrewharris1933@gmail.com>
2016-01-15 21:24:54 +0000

    test

---
+++ Photos.js
@@ -0,0 +1,184 @@
+var horizontalFiles = [ovcdRf4dh8HAEfgy,
+hlk269A7kZ1HlPnu,
+TBTYIbrus6QYJMcL,
+oTGoJERBiVJq2UcE,
+371GIpTlxoLfEwcY,
+RxUEj7Yf9ZGwi6kE,
+6GREmE6sA0Il1VcL,
+IBhDihwTO6zCGk1l,
+ehADZZGLbOfqmbLj,
+ZdNitmqVufRt418k,
+wA0gQjqJdf6g4W4p,
+qbj61AnFd7DIHWAx,
+sxMh0iTQC1YdfA8J,
+Sy20OR3FztUDMWTx,
+B1pSGgMunBZCMdbU,
+MFYKlU2Y0ygrsEeZ,
+PBhM4KwE2eO22MXi,
+iToQeiaDZyofOf4H,
+RiCNn2jpxaBKT83D,
+1FRwsTzBWPwGHnYo,
+YQuIhCl1C7Z0689e,
+4YvFgGGRR2YJoHEj,
+CJwWa8zPPRPVln6W,
+pQNeirASZPvjPqlK,
+EYDq9P6GfsmZuhI9,
+kYxck4Er19ygOGWc,
+EtsNnTwxtF4dftrm,
+JCV0xUzI9pd0QRc0,
+iHOJQkEceiLX78xl,
+g7QLqRCDCG693fyf,
+zzJhxcTu3MBMWIsL,
+CBiD8Y9fyxl4RZnR,
+ezIhnOvk26YorBYl,
+pU8Lb1eKcteS6NYR,
+OiFav3FSeO2c37RN,
+mbujc8SXjiWd7s0w,
+uvEskhTlHyJL91vn,
+tps8JqDt1yrL8VE0,
+r4yr1MVF9Z8hSdZy,
+q4utM8WZUt39AtBw,
+QgfxgdWUJVeWHaqK,
+cF7bLyISj8xngHHA,
+9H1XsjREyltaCSVb,
+sxQlcPmzsXkyYPgO,
+ogP3Qok9tA9Gnuds,
+sZoPTttoeVGiMuyu,
+SK0XRfKcP2zVCaoH,
+EF9PEt2eUVwqRZGU,
+uRzhDQZzoB0lxwzz,
+juwsXv6ty3c6bNc4,
+KgsnlwlptFbKgO2M,
+1Ah6I7fcgaYjwHaj,
+2AagJLg0KJZGsAia,
+faNbYcKseMyYQRPP,
+tQqTz1qN3LFkhxKG,
+lyow2F63dQ1miyvI,
+edKNmHmzVXSblTX3,
+6GUoKROIP3VD4ut2,
+EfkzX0Qbj0aAtwPM,
+EODTd3UBcNmWKbRn,
+p0vNa3suP8TYKTVn,
+KmGhDRXhspzCkU0d]
+
+var verticalFiles = [TyPi3KhK1QFVNY6n,
+hgg9QgFSlvTzXrhq,
+tuinvE3WwrchZuKJ,
+hmVobUTJH2k0MBpS,
+7ImxycFMYVUCTS9B,
+l2dFGAX0B0gUqERY,
+G2I9UsVZZ3nAImGD,
+vO8jgseWyyjOwH1r,
+kJOjprf6pIWvqSIc,
+Kglgir1IjTpcZi9x,
+toluuhRg6vpD3a3L,
+kl82iQTsLSsgBW7V,
+bls2YOcTVSuFuoG2,
+GG6WLptKhAQmN4IT,
+E3J0u8nXYoa2AR9G,
+v77sNPD14Mv0g9Aj,
+WnLBX1YjyC20NqZP,
+WuqVdKlZohM1vh6a,
+cY9fkN4daKLT13Ok,
+XLYvZXNe3qBWk1sT,
+bUVKtw6SVDxyyOOq,
+qJjCPGSJnc9pDOI0,
+zSEMXjezVkHBNKeg,
+8PfsEhrWZvjwUQp2,
+jwGAR0EXfHWfjKQj,
+4ynFb9qhvijIQ3Yl,
+oSNMOKhRnYvADr0t,
+CBtQ2vvSQlXfNqBJ,
+pC6Pw8ZH1V2kf1DF,
+ZRqiiwhgOHdHIq6u,
+fSYNf0Mde1v9e3nB,
+wgdM08AY2AobocGi,
+HNAufcHUdtOMN3Cx,
+MnXjuzk2kBWYElbo,
+RyxjiY6fN2odNgbj,
+CzLqBouQoHja4GnQ,
+PlFy0I7YyZZAAGy2,
+XU8BGZ9zMoWknv6K,
+67FjW7JzOEQnGx7r,
+EL4wtcYPulcGAApH,
+cG8ChdGVrsohfHne,
+vIH3z2TBII2ALr0E,
+hwMBHHoNXnReWM1k,
+Dd42MwKXdHk8MHc6,
+rO00Epmoy0WdB2kc,
+l7y0SHGWmAuo2IMU,
+sU9LTqphr2dOib3N,
+fmm3LXizatzdmyH1,
+qh79JBguinlle2Pm,
+7yfg7DzAZ6FxN468,
+rGXxgF0COop7WJpi,
+LzAfsu8HsqOheN81,
+xq0wzo6fT2v2YFwa,
+1PVTQyPbIzgtSEfa,
+phHMpsN4ef9m9VGO,
+68lsB6h6pjeJkOLW,
+Q98gtBhFokNVnfsn,
+F68ND1n9Iz6cnSgS,
+QTSD0IL6TfCKSPDd,
+T1NAbvjJzsnTh2sq,
+B4jpJ43uBmURJL33,
+siH7jk7MiUlZgBNM,
+TnujsHgkzDWfCQE9]
+
+
+var Photos = (function() {
+	
+	function GetHTML() {
+		var html = "";
+		var htmlFiles = [];
+		
+		for (var photo in horizontalFiles) {
+			var a = '<a href="https://copy.com/' + photo + '" data-size="1024x682">\
+         			<img src="https://copy.com/' + photo + '"/>\
+         			</a>';
+         	htmlFiles.add(a);
+		}
+		
+		for (var photo in verticalFiles) {
+			var a = '<a href="https://copy.com/' + photo + '" data-size="682x1024">\
+         			<img src="https://copy.com/' + photo + '"/>\
+         			</a>';
+         	htmlFiles.add(a);
+		}
+		
+		shuffle(htmlFiles);
+		
+		for (var photo in htmlFiles){
+			html += photo;
+		}
+		
+		$('#my-gallery').html(html);
+	}
+	
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex;
+	
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+	
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+	
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+	
+	  return array;
+	}
+	
+	
+	return {
+        Init: function() {
+            GetHTML();
+            initPhotoSwipeFromDOM('.my-gallery');
+        }
+    };
+})();
